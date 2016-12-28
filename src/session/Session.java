b137c79372548b8217b18e8c82daad01f801dca9
Juan Carlos Cuevas Martínez <jccuevas@ujaen.es>
2016-12-28 12:04:49 +0100

    Añadido comentarios

--- src/session/Session.java
+++ src/session/Session.java
@@ -16,6 +16,12 @@ public class Session {
     public static final String SID="SID";
    
 
+    /**
+     * Devuelve la cadena para el cliente formateada
+     * @param user Nombre de usuario
+     * @param password Clave del usuario
+     * @return 
+     */
     public static String getNewSession(String user, String password) {
         String key = new String(java.util.Base64.getEncoder().encode(password.getBytes()));
         String expires = SessionManager.newSession();
